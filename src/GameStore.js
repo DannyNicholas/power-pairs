5b8eed5cca9abe437d58c8a22e5e9349e119335a
DannyNicholas <dannynicholas23@googlemail.com>
2017-08-23 00:07:05 +0100

    Update Redux Dev Tools for browser extension

--- src/GameStore.js
+++ src/GameStore.js
@@ -1,9 +1,13 @@
 import { createStore } from 'redux'
-import devToolsEnhancer from 'remote-redux-devtools'
+//import devToolsEnhancer from 'remote-redux-devtools'
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import CardReducer from './reducers/CardReducer'
 
 const GameStore = createStore(
     CardReducer,
+
+    /* For Redux dev tools see... */
+    /* https://github.com/zalmoxisus/redux-devtools-extension#usage */
     devToolsEnhancer()
 )
 
