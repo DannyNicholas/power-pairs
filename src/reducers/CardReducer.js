feae5f2fc6a3a843e3e68c1ff3eec5b4c1ae1c89
DannyNicholas <dannynicholas23@googlemail.com>
2017-08-23 23:42:14 +0100

    Reducer automatically flips cards back when 2 are shown

--- src/reducers/CardReducer.js
+++ src/reducers/CardReducer.js
@@ -3,6 +3,7 @@ import { fromJS } from 'immutable'
 import CardState from '../constants/CardState'
 import CardAction from '../constants/CardAction'
 import CardType from '../constants/CardType'
+import CardActionCreators from '../action-creators/CardActionCreators'
 
 const initialState = fromJS({
     cards: [
@@ -41,13 +42,38 @@ const initialState = fromJS({
     ]
 })
 
+const visibleCards = (cards) => {
+    return cards
+        .filter(
+            card => (card.get('cardState') === CardState.FACE_UP))
+}
+
+const flippingAndVisibleCards = (cards) => {
+    return cards
+        .filter(
+            card => (
+                card.get('cardState') === CardState.FACE_UP) ||
+                card.get('nextCardState') === CardState.FACE_UP)
+}
+
 const startCardFlip = (state, action) => {
+
     // find index of wanted card id
     const index = state.get('cards').findIndex(card => card.get('id') === action.id)
     
     // get card and add card's next state after being flipped
     const card = state.get('cards').get(index)
     const nextState = card.get('cardState') === CardState.FACE_DOWN ? CardState.FACE_UP : CardState.FACE_DOWN
+    
+    // stop card flip to face-up if we already have 2 face-up
+    if (nextState === CardState.FACE_UP) {
+        const activeCards = flippingAndVisibleCards(state.get('cards'))
+        if (activeCards.count() === 2) {
+            console.warn("A maximum of 2 cards can flip.")
+            return state
+        }
+    }
+    
     const newCard = card.set('nextCardState', nextState).set('isFlipping', true)
 
     // replace old card
@@ -72,6 +98,22 @@ const completeCardFlip = (state, action) => {
     return state.set('cards', newCards)
 }
 
+const revertFlippedCards = (state) => {
+    // if 2 are visible revert them to face-down
+    const faceUpCards = visibleCards(state.get('cards'))
+    if (faceUpCards.count() === 2) {
+        console.warn("2 cards have been flipped. Revert them.")
+        let newState = state
+        faceUpCards.forEach(card => {
+            newState = startCardFlip(
+                newState,
+                CardActionCreators.flipCardStart(card.get('id')))
+        })
+        return newState
+    }
+    return state
+}
+
 const CardReducer = (state = initialState, action) => {
     
     console.log(action)
@@ -81,7 +123,8 @@ const CardReducer = (state = initialState, action) => {
             return startCardFlip(state, action)
 
         case CardAction.FLIP_CARD_COMPLETED:
-            return completeCardFlip(state, action)
+            const flipCompleteState = completeCardFlip(state, action)
+            return revertFlippedCards(flipCompleteState)
            
         default:
             return state
